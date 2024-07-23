52dd6bb7d481a6dd2ae0132debcc1c2decf05d99
Mohammad Sobhy <31019449+MohamadSobhy@users.noreply.github.com>
2024-07-23 15:38:09 +0100

    Comments add to clarify the logic of the calculateTotalCheckoutPrice() method.

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -29,19 +29,33 @@ class Checkout {
     _products.update(productCode, (value) => value + 1, ifAbsent: () => 1);
   }
 
+  /// A method to calculate the total price of the scanned products based on the pricing rules.
   num calculateTotalCheckoutPrice() {
+    /// A variable to store the total price of the scanned products.
     num total = 0;
 
+    /// Creates a copy of the scanned products map to avoid any changes on the original map.
     final totalProducts = {..._products};
 
+    /// Iterate over the scanned products untill the products list get empty
+    /// and the total price is calculated.
     while (!totalProducts.isEmptyOrZeros) {
+      /// Iterate over the pricing rules list to apply the rules on the scanned products.
+      /// The rules will be applied based on the order of the pricing rule type.
+      /// [Offers Rules] will be applied first if exist then the [Individual Rules] will be applied.
       for (final rule in pricingRules) {
+        /// Finish the loop and return the total price
+        /// if the products list get empty before applying all the pricing list.
+        /// Products list is empty means that the total price is calculated for all the
+        /// scanned products so no need to continue applying the rest of pricing rules.
         if (totalProducts.isEmptyOrZeros) return total;
 
+        /// Get the total quantity of the product code in the scanned products list.
         int totalQuantity = totalProducts[rule.productCodes.first] ?? 0;
 
         print('Before: $totalProducts');
 
+        /// Apply the pricing rule logic based on the type of the rule.
         switch (rule.type) {
           case PricingRuleType.mealDeal:
             final mealDealProducts = rule.productCodes;
@@ -49,6 +63,8 @@ class Checkout {
             bool hasADeal = true;
 
             while (hasADeal) {
+              /// Checks if the deal products exist in the scanned products list or not.
+              /// If not exist, then break the loop and stop applying the meal deal rule.
               for (final code in mealDealProducts) {
                 if (!totalProducts.containsKey(code) ||
                     totalProducts[code]! <= 0) {
@@ -57,9 +73,12 @@ class Checkout {
                 }
               }
 
+              /// If the deal products exist in the scanned products list,
+              /// then apply the meal deal rule.
               if (hasADeal) {
                 total += rule.price;
 
+                /// Decrease the quantity of the deal products by 1 after applying the meal deal rule.
                 for (final code in mealDealProducts) {
                   totalProducts[code] = totalProducts[code]! - 1;
                 }
@@ -68,17 +87,29 @@ class Checkout {
 
             break;
           case PricingRuleType.buyNGet1Free:
+
+            /// Iterate while the total quantity of the product code
+            /// is greater than the quantity of the rule.
+            /// The rule will be applied on the product code based on the quantity of the rule.
+            /// After applying the rule, decrease the total quantity of the product
+            /// by the quantity of the rule and the free quantity.
             while (totalQuantity >= rule.quantity) {
               total += rule.price * rule.quantity;
               totalQuantity -= rule.quantity + rule.freeQuantity;
             }
 
+            /// Reset the product quantity to the remaining quantity after applying the rule.
             totalProducts[rule.productCodes.first] = totalQuantity;
 
             break;
           case PricingRuleType.multiPriced:
+
+            /// Calcualte how many offers of this rule could be applied to the product.
             final offersCount = totalQuantity ~/ rule.quantity;
 
+            /// If there are offers of this rule could be applied to the product,
+            /// then calculate the total price of the offers
+            /// and reset the product quantity to the remaining quantity.
             if (offersCount > 0) {
               final remainingQuantity = totalQuantity % rule.quantity;
               total += offersCount * rule.price;
@@ -86,6 +117,9 @@ class Checkout {
             }
             break;
           case PricingRuleType.individual:
+
+            /// Calculate the total price of the product based on the quantity of the product
+            /// and reset the product quantity to 0.
             total += rule.price * totalQuantity;
             totalProducts[rule.productCodes.first] = 0;
             break;
