8038291c185d36f050ad6ef41edda66baba8f74e
Mohammad Sobhy <31019449+MohamadSobhy@users.noreply.github.com>
2024-07-23 16:04:20 +0100

    Test Case added to test a sample example.

--- test/fluro_task_test.dart
+++ test/fluro_task_test.dart
@@ -1,8 +1,46 @@
-import 'package:fluro_task/fluro_task.dart';
+import 'package:fluro_task/checkout.dart';
+import 'package:fluro_task/models/pricing_rule.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test(
+    'calculateCheckoutKataSample should calculate the total price of the scanned products based on the pricing rules',
+    () async {
+      // arrange
+
+      final pricingRules = [
+        PricingRule.individual(productCode: 'A', price: 0.50),
+        PricingRule.individual(productCode: 'B', price: 0.75),
+        PricingRule.individual(productCode: 'C', price: 0.25),
+        PricingRule.individual(productCode: 'D', price: 1.5),
+        PricingRule.individual(productCode: 'E', price: 2),
+        PricingRule.multiPriced(quantity: 2, productCode: 'B', price: 1.25),
+        PricingRule.buyNGet1Free(quantity: 3, productCode: 'C', price: 0.25),
+        PricingRule.mealDeal(productCodes: ['D', 'E'], price: 3),
+      ];
+
+      // act
+      final checkout = Checkout(pricingRules);
+
+      checkout.scan('A');
+      checkout.scan('B');
+      checkout.scan('C');
+      checkout.scan('D');
+      checkout.scan('E');
+      checkout.scan('A');
+      checkout.scan('B');
+      checkout.scan('A');
+      checkout.scan('A');
+      checkout.scan('E');
+      checkout.scan('D');
+      checkout.scan('C');
+      checkout.scan('C');
+      checkout.scan('C');
+
+      final total = checkout.calculateTotalCheckoutPrice();
+
+      // assert
+      expect(total, 10);
+    },
+  );
 }
